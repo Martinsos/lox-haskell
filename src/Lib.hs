70f5ee4a05f9b5cd0b5f5b05b0dcb57857caf482
Martin Sosic <sosic.martin@gmail.com>
2020-03-01 23:07:23 +0100

    (Book: 4.1.1): Added simple error reporting logic.

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,8 @@ module Lib
     , scanTokens
     ) where
 
+import System.IO (hPutStrLn, stderr)
+
 run :: String -> IO ()
 run source = do
   let tokens = scanTokens source
@@ -11,3 +13,13 @@ run source = do
 
 scanTokens :: String -> [String]
 scanTokens source = concat $ map words $ lines $ source
+
+error :: Int -> String -> IO ()
+error line message = report line "" message
+
+-- NOTE: In book, he also sets global hadError flag to true here, and uses that to check if error
+--   happened after being done with execution of program.
+--   I could achieve smth like that right now by introducing Lox (or App) monad instead of IO, but I will leave that
+--   for later to see if there is a simpler option.
+report :: Int -> String -> String -> IO ()
+report line whereMessage message = hPutStrLn stderr $ "[line " ++ (show line) ++ "] Error" ++ whereMessage ++ ": " ++ message
